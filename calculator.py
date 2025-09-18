ac111bf6ffd712e580c9b9e2b481c0ecfc0490cc
ham0089 <Laurencecatalan326@yahoo.com>
2025-09-18 20:55:16 +0800

    just add new

--- calculator.py
+++ calculator.py
@@ -1,6 +1,6 @@
 print ('Welcome to the Calculator Program!')
 num1 = float(input('Enter first number: '))
-op = input('Enter operator (+, -, *, /): ')
+op = input('Enter operator (+, -, *, /,**,//): ')
 num2 = float(input('Enter second number: '))
 
 
